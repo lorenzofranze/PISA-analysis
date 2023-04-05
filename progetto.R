e422d2809db9c44d1cd1acc355a54325fe7763e9
bzamboni <126112040+bzamboni@users.noreply.github.com>
2023-04-05 12:38:54 +0200

    Add files via upload

--- progetto.R
+++ progetto.R
@@ -2,7 +2,7 @@
 library(foreign)
 schools = read.spss("CY07_MSU_SCH_QQQ.sav", to.data.frame=TRUE)
 students = read.spss("CY07_MSU_STU_QQQ.sav", to.data.frame=TRUE)
-teachers = read.spss("CY07_MSU_TCH_QQQ.sav", to.data.frame=TRUE) 
+teachers = read.spss("CY07_MSU_tch_QQQ.sav", to.data.frame=TRUE) 
 
 # dataframe about students
 n_stud = dim(students)[1]
@@ -10,9 +10,9 @@ p_stud = dim(students)[2]
 # 1119 variables: way too many
 head(students)
 
+###################################################################################
 students_career = students[,c(1:14,430:511)]
 dim(students_career)
-head(students_career)
 
 # prova con aggregati
 # select interesting columns
@@ -26,7 +26,7 @@ prova = na.omit(prova)
 dim(prova)
 
 # some plots
-quartz()
+x11()
 boxplot(prova)
 
 x11()
@@ -44,7 +44,9 @@ head(df)
 df = na.omit(df)
 dim(df)
 
-# change column names
+# select studenti italiani
+df = df[df$CNTRYID=='Italy',]
+dim(df)
 
 # plots
 x11()
@@ -53,94 +55,39 @@ boxplot(df)
 x11()
 pairs(df)
 
-attach(df)
-class(BSMJ)
-df$BSMJ <- as.numeric(BSMJ)
-class(EMOSUPS)
-df$EMOSUPS <- as.numeric(EMOSUPS)
-df$PERCOMP <- as.numeric(PERCOMP)
-df$PERCOOP <- as.numeric(PERCOOP)
-df$ATTLNACT <- as.numeric(ATTLNACT)
-df$COMPETE <- as.numeric(COMPETE)
-df$GFOFAIL <- as.numeric(GFOFAIL)
-df$RESILIENCE <- as.numeric(RESILIENCE)
-df$MASTGOAL <- as.numeric(MASTGOAL)
+###################################################################################
+# MANOVA ATTEMPT
 
-model1 = lm(PV1MATH ~ BSMJ + 
-              EMOSUPS + 
-              PERCOMP + 
-              PERCOOP + 
-              ATTLNACT + 
-              COMPETE + 
-              GFOFAIL + 
-              RESILIENCE + 
-              MASTGOAL, data = df)
-summary(model1)
+# select interesting variables
+df = students[,c(1,863,887,895,896,898,900,903,904,1027,1037)]
+head(df)
+dim(df)
 
-model2 = lm(PV1MATH ~ BSMJ + 
-              EMOSUPS + 
-              PERCOMP + 
-              PERCOOP + 
-              ATTLNACT + 
-              COMPETE + 
-              GFOFAIL + 
-              RESILIENCE, data = df)
-summary(model2)
+# remove NA's
+df = na.omit(df)
+dim(df)
 
-model3 = lm(PV1MATH ~ BSMJ + 
-              EMOSUPS + 
-              PERCOMP + 
-              PERCOOP + 
-              ATTLNACT + 
-              COMPETE + 
-              GFOFAIL, data = df)
-summary(model3)
+# change column names
+colnames(df) <- c('country', 'exp_occupation', 'parents_support', 'compet_school',
+                  'coop_school', 'compet', 'fear_fail', 'resilience',
+                  'master_orient', 'math', 'reading')
 
-model4 = lm(PV1MATH ~ BSMJ + 
-              EMOSUPS + 
-              PERCOMP + 
-              PERCOOP +
-              COMPETE + 
-              GFOFAIL, data = df)
-summary(model4)
+head(df)
 
-model5 = lm(PV1MATH ~ BSMJ + 
-              EMOSUPS + 
-              PERCOOP + 
-              COMPETE + 
-              GFOFAIL, data = df)
-summary(model5)
+library(MVN)
+library(car)
+library(heplots)
 
-####################
-## Dopo aver selezionato le domande rimuoviamo i dati mancanti
-## Verifico che CNTRYID è un factor ed estraggo i livelli
-## Salvo i livelli in una lista e rimuovo quelli che non hanno neanche
-## una coppia valida
-## Creo una lista [key,value] = [paese,R^2_adj] vuota
-## estraggo da df un singolo paese e lo salvo in df2
-## creo il modello lineare con le variabili salvate come as.numeric()
-## inserisco nella lista il valore
+### factors: countries --> too many (?)
 
-df <- na.omit(df)
-class(CNTRYID)
-levels(CNTRYID)
+### check normality in each population
 
+# univariate
 
-countries <- levels(df$CNTRYID)
-countries <- countries[-c(6,12,18,33,40,53,66,75,80)]
-R_adj.values <- list()
-for (c in countries){
-  df2 = df[df$CNTRYID==c,]
-  model1 = lm(PV1MATH ~ BSMJ + 
-                EMOSUPS + 
-                PERCOMP + 
-                PERCOOP + 
-                ATTLNACT + 
-                COMPETE + 
-                GFOFAIL + 
-                RESILIENCE + 
-                MASTGOAL, data = df2)
-  R_adj.values[[c]] <- summary(model1)$adj.r.squared
-}
-R_adj.values
+# multivariate
 
+### check omoschedasticity: rule of thumb
+
+### MANOVA
+
+### further analysis
